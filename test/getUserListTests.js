bbf5fbe365ef83007fd3780a246197bbb2972adf
d.vinokurova <d.vinokurova@fasten.com>
2020-10-22 17:05:59 +0300

    Add allure report

--- test/getUserListTests.js
+++ test/getUserListTests.js
@@ -7,17 +7,17 @@ let server = 'https://reqres.in/api';
 chai.use(chaiHttp);
 
 describe('Get list of users', () => {
-    it('it should receive all users list', () => {
-        chai.request(server)
-        .get('/users')
-        .query({page: 2})
-        .then((res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('Object');
-            page = JSON.parse(res.text).page;
-            //add check response body 
-            //add check size of user list 
-            assert.equal(2, page);
-        });
-    });
+  it('it should receive all users list', () => {
+    chai.request(server)
+      .get('/users')
+      .query({ page: 2 })
+      .then((res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('Object');
+        page = JSON.parse(res.text).page;
+        //add check response body 
+        //add check size of user list 
+        assert.equal(2, page);
+      });
+  });
 });
--- test/putUpdateUserTests.js
+++ test/putUpdateUserTests.js
@@ -4,6 +4,8 @@ let chaiHttp = require('chai-http');
 const { assert } = require('chai');
 let server = 'https://reqres.in/api';
 
+require("mocha-allure-reporter");
+
 chai.use(chaiHttp);
 
 describe('Update user info', () => {
@@ -14,23 +16,23 @@ describe('Update user info', () => {
         }
 
         chai.request(server)
-        .post('/users')
-        .send(userReqBody)
-        .then((res) => {
-            id = JSON.parse(res.text).id;
-        });
+            .post('/users')
+            .send(userReqBody)
+            .then((res) => {
+                id = JSON.parse(res.text).id;
+            });
         let updateReqBody = {
             name: "user change",
             job: "user job change"
         }
         chai.request(server)
-        .put('/users/${id}')
-        .send(updateReqBody)
-        .then((res) => {
-            res.should.have.status(200);
-            res.body.should.have.property('name').equals('user change');
-            res.body.should.have.property('job').equals('user job change');
-            res.body.should.have.property('updatedAt');
-        });
+            .put('/users/${id}')
+            .send(updateReqBody)
+            .then((res) => {
+                res.should.have.status(200);
+                res.body.should.have.property('name').equals('user change');
+                res.body.should.have.property('job').equals('user job change');
+                res.body.should.have.property('updatedAt');
+            });
     });
 });
